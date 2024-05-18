e7a25c9e8132f34f5b41f4dc464311cccfb4ff9e
Aituglo <contact@aituglo.com>
2024-05-18 16:36:11 +0200

    basic dashboard with basic auth

---
+++ README.md
@@ -0,0 +1,17 @@
+# Huntly
+
+The Bug Bounty Hunter Dashboard.
+
+
+## Getting Started
+
+Follow these steps to clone the repository and start the development server:
+
+- `git clone https://github.com/Aituglo/huntly`
+- `npm install`
+- Create a `.env.local` file by copying the example environment file:
+  `cp env.example.txt .env.local`
+- Add the required environment variables to the `.env.local` file.
+- `npm run dev`
+
+You should now be able to access the application at http://localhost:3000.
---
+++ components/recent-bounty.tsx
@@ -0,0 +1,46 @@
+
+export function RecentBounty() {
+  return (
+    <div className="space-y-8">
+      <div className="flex items-center">
+        <div className="space-y-1">
+          <p className="text-sm font-medium leading-none">Yahoo</p>
+          <p className="text-sm text-muted-foreground">
+            Hackerone
+          </p>
+        </div>
+        <div className="ml-auto font-medium">+$1,999.00</div>
+      </div>
+      <div className="flex items-center">
+        <div className="space-y-1">
+          <p className="text-sm font-medium leading-none">Infomaniak</p>
+          <p className="text-sm text-muted-foreground">Yes We Hack</p>
+        </div>
+        <div className="ml-auto font-medium">+$390.00</div>
+      </div>
+      <div className="flex items-center">
+        <div className="space-y-1">
+          <p className="text-sm font-medium leading-none">Open Sea</p>
+          <p className="text-sm text-muted-foreground">
+            Hackerone
+          </p>
+        </div>
+        <div className="ml-auto font-medium">+$200000.00</div>
+      </div>
+      <div className="flex items-center">
+        <div className="space-y-1">
+          <p className="text-sm font-medium leading-none">Booking</p>
+          <p className="text-sm text-muted-foreground">Hackerone</p>
+        </div>
+        <div className="ml-auto font-medium">+$3000.00</div>
+      </div>
+      <div className="flex items-center">
+        <div className="space-y-1">
+          <p className="text-sm font-medium leading-none">Doctolib</p>
+          <p className="text-sm text-muted-foreground">Yes We Hack</p>
+        </div>
+        <div className="ml-auto font-medium">+$2000.00</div>
+      </div>
+    </div>
+  );
+}
